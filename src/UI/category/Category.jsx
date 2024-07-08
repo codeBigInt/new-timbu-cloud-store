0628eb49609c6e5dc2a181df3c1acddd87d940cc
Lucky Elliot <elliotlucky99@gmail.com>
2024-07-08 21:30:04 +0100

    hmmm

--- src/UI/category/Category.jsx
+++ src/UI/category/Category.jsx
@@ -4,9 +4,9 @@ import { mediaContext } from '../../context/mediaContext'
 
 
 const Category = ({image, text, key}) => {
-  const {handleCategoryClick} = useContext(mediaContext)
+  const {handleCategoryClick, selectedCategory} = useContext(mediaContext)
   return (
-    <li onClick={() => handleCategoryClick(text)} key={key} className={`${text === 'All' ? styles.one : ''} ${styles.container}`}>
+    <li onClick={() => handleCategoryClick(text)} key={key} className={`${text === selectedCategory ? styles.one : ''} ${styles.container}`}>
         <img className={styles.img} src={image} alt="" />
         <span className={styles.text}>{text}</span>
     </li>
