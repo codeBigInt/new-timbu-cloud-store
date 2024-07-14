8bee1a48bedad69bfbb177845df1c12d5a124283
Lucky Elliot <elliotlucky99@gmail.com>
2024-07-14 01:41:01 +0100

    Last commit

--- src/UI/notificationModal/Notification.jsx
+++ src/UI/notificationModal/Notification.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./notification.module.css";
-import { FaRegBellSlash } from "react-icons/fa";
+import { FaRegBellSlash, FaRegTimesCircle } from "react-icons/fa";
 
 const Notification = ({ text }) => {
   return (
     <div className={styles.notifier}>
       <span className={styles.close}>
-        <FaRegBellSlash />
+        <FaRegTimesCircle />
       </span>
       <span>{text}</span>
     </div>
